36342442c593fcbebab0664eaa341fa3d11f07ac
Руслан Бикбаев <ruslan@MacBook-Air-Ruslan.local>
2025-10-06 13:37:03 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import InteractiveMap from './MapObject'
+
+function App() {
+
+  return (
+   <InteractiveMap />
+  )
+}
+
+export default App

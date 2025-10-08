f750df3d444a30fddb458ee2ce8ac305b40a073e
Ruscore-Main <oobitovich@mail.ru>
2025-10-08 11:46:05 +0300

    fix commit

--- src/KremlinMap.tsx
+++ src/KremlinMap.tsx
@@ -1,6 +1,8 @@
 import { AnimatePresence, motion } from "framer-motion";
-import React, { useRef, useState } from "react";
+import { useRef, useState, type JSX } from "react";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import KulSharif from "./assets/kul-sharif.svg?react";
+import Wall from "./assets/wall.svg?react";
 
 type MapObject = {
   id: number;
@@ -9,34 +11,35 @@ type MapObject = {
   left: number; // %
   width: number; // %
   category: string;
+  src?: string;
 };
 
 const mapObjects: MapObject[] = [
-  { id: 1, name: "Музей 1", top: 16.5, left: 30, width: 5, category: "museum" },
+  {
+    id: 1,
+    name: "Кул Шариф",
+    top: 175,
+    left: 599,
+    width: 101,
+    category: "museum",
+    src: "map-objects/kul-sharif.webp",
+  },
   {
     id: 2,
-    name: "Музей 2",
-    top: 42,
-    left: 50,
-    width: 5,
-    category: "museum",
-  },
-  {
-    id: 3,
-    name: "Стена",
-    top: 66,
-    left: 33,
-    width: 4,
+    name: "Нижняя стена",
+    top: 456,
+    left: 394,
+    width: 279,
     category: "wall",
+    src: "map-objects/wall.png",
   },
-  { id: 4, name: "Вход", top: 50, left: 88, width: 2, category: "entrance" },
 ];
 
 export const KremlinMap = () => {
   const [activeId, setActiveId] = useState<number | null>(null);
   const [hovered, setHovered] = useState<MapObject | null>(null);
   const [filter, setFilter] = useState<string | null>(null);
-  const [popupVisible, setPopupVisible] = useState(false);
+  const [_, setPopupVisible] = useState(false);
 
   const transformRef = useRef<any>(null);
   const contentRef = useRef<HTMLDivElement>(null);
@@ -48,17 +51,16 @@ export const KremlinMap = () => {
   const handleSelectObject = (obj: MapObject) => {
     setActiveId(obj.id);
     setPopupVisible(true);
+
     if (!transformRef.current || !contentRef.current) return;
 
     const zoom = 3;
     const content = contentRef.current;
-    if (!content) return;
-
     const contentWidth = content.offsetWidth;
     const contentHeight = content.offsetHeight;
 
-    const x = (obj.left / 100) * contentWidth;
-    const y = (obj.top / 100) * contentHeight;
+    const x = obj.left;
+    const y = obj.top;
 
     transformRef.current.setTransform(
       contentWidth / 2 - x * zoom,
@@ -124,72 +126,87 @@ export const KremlinMap = () => {
           </ul>
         </div>
       </div>
-      <TransformWrapper ref={transformRef}>
-        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+      <TransformWrapper ref={transformRef} maxScale={3} minScale={0.9}>
+        {({ zoomIn, zoomOut, resetTransform }) => (
           <div className="relative mx-auto" ref={contentRef}>
             <div className="flex flex-col flex-wrap gap-[4px] absolute top-[32px] right-[32px] z-[2]">
               <button onClick={() => zoomIn()}>+</button>
               <button onClick={() => zoomOut()}>-</button>
               <button onClick={() => resetTransform()}>x</button>
             </div>
-            <TransformComponent>
+            <TransformComponent
+              contentStyle={{
+                width: "initial",
+                height: "100%",
+              }}
+              wrapperStyle={{
+                width: "initial",
+                height: "100%",
+                transform: "translate3d(0,0,0)",
+              }}
+            >
               <div className="relative overflow-hidden h-full w-full outline-0">
                 <img
-                  src="/kremlin-map-image.png"
+                  src="kremlin-map-image25x.webp"
                   alt="map"
                   className="w-full"
                 />
 
                 {mapObjects.map((obj) => {
-                  const isVisible = !filter || obj.category === filter;
-                  const isActive = obj.id === activeId;
+                  const isVisible = filter === null || obj.category === filter;
 
                   return (
                     <div
                       key={obj.id}
+                      className="absolute"
+                      style={{
+                        top: `${obj.top}px`,
+                        left: `${obj.left}px`,
+                        transform: "translate(-50%, -50%)",
+                        width: `${obj.width}px`,
+                        zIndex: 10,
+                        opacity: isVisible ? 1 : 0,
+                        pointerEvents: isVisible ? "auto" : "none",
+                        cursor: isVisible ? "pointer" : "default",
+                      }}
                       onMouseEnter={() => setHovered(obj)}
                       onMouseLeave={() => setHovered(null)}
-                      onClick={() => handleSelectObject(obj)}
-                      className="absolute rounded-full transition-all duration-500 cursor-pointer"
-                      style={{
-                        top: `${obj.top}%`,
-                        left: `${obj.left}%`,
-                        width: `${obj.width}%`,
-                        height: `${obj.width}%`,
-                        backgroundColor: isVisible ? "#3b82f6" : "#555",
-                        opacity: isVisible ? 1 : 0.4,
-                        transform: "translate(-50%, -50%)",
-                        zIndex: 10,
-                        animation: isActive ? "pulse 2s infinite" : undefined,
-                      }}
-                    />
+                    >
+                      <div className="relative">
+                        <AnimatePresence>
+                          {hovered && hovered.id === obj.id && (
+                            <motion.div
+                              initial={{ opacity: 0, y: 10 }}
+                              animate={{ opacity: 1, y: 0 }}
+                              exit={{ opacity: 0, y: 10 }}
+                              transition={{ duration: 0.2 }}
+                              className="absolute flex w-full justify-center pointer-events-none z-20 "
+                              style={{
+                                top: "-40px",
+                                fontSize: "clamp(6px, 16px)",
+                              }}
+                            >
+                              <span className="px-1 py-1 bg-black/80 rounded shadow-lg ">
+                                {hovered.name}
+                              </span>
+                            </motion.div>
+                          )}
+                        </AnimatePresence>
+                        <img
+                          onClick={() => handleSelectObject(obj)}
+                          src={obj.src}
+                          className="object-contain duration-500 cursor-pointer"
+                        />
+                      </div>
+                    </div>
                   );
                 })}
-
-                <AnimatePresence>
-                  {hovered && (
-                    <motion.div
-                      initial={{ opacity: 0, y: 10 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      exit={{ opacity: 0, y: 10 }}
-                      transition={{ duration: 0.2 }}
-                      className="absolute px-3 py-2 bg-black/80 text-sm rounded shadow-lg pointer-events-none z-20"
-                      style={{
-                        left: `${hovered.left}%`,
-                        top: `${hovered.top}%`,
-                        transform: "translate(-50%, -130%)",
-                        fontSize: "clamp(10px, 1.2vw, 16px)",
-                      }}
-                    >
-                      {hovered.name}
-                    </motion.div>
-                  )}
-                </AnimatePresence>
               </div>
             </TransformComponent>
           </div>
         )}
       </TransformWrapper>
+
       <style>{`
                 @keyframes pulse {
                     0% { box-shadow: 0 0 0 0 rgba(59, 130, 246, 0.5); }
